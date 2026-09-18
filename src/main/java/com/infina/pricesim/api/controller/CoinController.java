05e5d93379127bc39b6b20730de61c0426f64e45
Yusuf GUNES <yusufguness1.00@gmail.com>
2026-09-18 21:51:42 +0300

    feat: project can run with docker

--- src/main/java/com/infina/pricesim/api/controller/CoinController.java
+++ src/main/java/com/infina/pricesim/api/controller/CoinController.java
@@ -3,9 +3,7 @@ package com.infina.pricesim.api.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.infina.pricesim.service.CoinService;
 import com.infina.pricesim.state.SafeCoinState;
